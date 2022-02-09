0fc4a85a1c9ca4de0cbb41fb4e8f5d0f0cdd0883
it21299766 <99312863+it21299766@users.noreply.github.com>
2022-02-09 07:59:04 +0000

    completed 3

--- Tute03.c
+++ Tute03.c
@@ -11,3 +11,32 @@ sum = 1+2+3+....+ 99+100 = 5050
 n -> 1-
 sum = 1+2+3+...+10 = 55 */
 
+#include <stdio.h>
+
+int main() {
+  int sum=0,j,i,n;
+
+  //INPUT
+  for (j=0;j<1;j++)
+  {
+    printf("Enter a number : ");
+    scanf("%d",&n);
+    
+    //VALIDATION
+    if (n<0){
+      printf("Entered a wrong value, please try again later\n");
+      j--;
+    }
+
+  }
+
+  //CALCULATION
+  for (i=1;i<=n;i++){
+    sum=sum+i;
+  }
+
+  //OUTPUT
+  printf("Sum = %d",sum);
+  
+  return 0;
+}
